6cfe5afa5275591e1aba9b8dfe34929ccacf76ad
jochongs <joch2712@naver.com>
2025-05-19 10:42:39 +0900

    chore: Update favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteFaviconsPlugin("favicon.png")],
   server: {
     host: true,
     port: 5173,

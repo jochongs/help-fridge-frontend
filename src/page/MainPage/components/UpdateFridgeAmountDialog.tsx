02c8256c13a73414917cb0441a919748679cec3e
jochongs <joch2712@naver.com>
2025-05-19 16:15:40 +0900

    feat: Update input with size

--- src/page/MainPage/components/UpdateFridgeAmountDialog.tsx
+++ src/page/MainPage/components/UpdateFridgeAmountDialog.tsx
@@ -169,7 +169,7 @@ export default function UpdateFridgeAmountDialog({
                   type="text"
                   className={cn(
                     "caret-transparent h-8.5 text-xl rounded-sm bg-[#F0F0F0] mr-1.5 text-[#494949] flex justify-center items-center text-center focus:outline-none",
-                    fridge.amount.toString().length >= 4 ? "w-12" : "w-8",
+                    fridge.amount.toString().length >= 4 ? "w-13" : "w-8",
                   )}
                   value={amountInput}
                   onChange={(e) => {
